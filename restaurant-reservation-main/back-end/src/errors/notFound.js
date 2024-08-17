525ac7f4fc0791f7f6c2e60776a546ccfa42d92c
Shaun Kirk <158525428+shakeableabyss@users.noreply.github.com>
2024-08-17 18:15:32 -0400

    Add files via upload

--- restaurant-reservation-main/back-end/src/errors/notFound.js
+++ restaurant-reservation-main/back-end/src/errors/notFound.js
@@ -2,8 +2,7 @@
  * Express API "Not found" handler.
  */
 function notFound(req, res, next) {
-  console.log(req.originalUrl)
-  next({ status: 404, message: `!Path not found: ${req.originalUrl}` });
+  next({ status: 404, message: `Path not found: ${req.originalUrl}` });
 }
 
 module.exports = notFound;
